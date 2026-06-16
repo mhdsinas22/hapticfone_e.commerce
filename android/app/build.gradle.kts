8353e7a3037795a4e3a695240363ff6fc251f2be
mhdsinas22 <sinasmuhammed2@gmail.com>
2026-06-16 09:26:15 +0530

    jun 16 just look the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,13 +18,13 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.sinas.hapticfone"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
         defaultConfig {
         applicationId = "com.sinas.hapticfone"
-        minSdk = 23
-        targetSdk = 35
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
         versionCode = 2
         versionName = "1.0.1"
     }
@@ -41,24 +41,19 @@ android {
    
         signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
         }
     }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-                 signingConfig = signingConfigs.getByName("release")
+                 signingConfig = signingConfigs.getByName("debug")
         }
    
     }
 }
 
 dependencies {
-  implementation("net.sf.proguard:proguard-annotations:6.2.2")
    implementation("com.google.android.material:material:1.12.0")
 
 }
